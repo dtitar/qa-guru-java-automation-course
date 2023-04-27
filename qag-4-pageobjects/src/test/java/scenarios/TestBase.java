09a484d21d99049905b566ede7d54029f9d0472d
dtitar <dmitry.titar@gmail.com>
2023-04-27 15:17:33 +0300

    added lesson 4

---
+++ qag-4-pageobjects/src/test/java/scenarios/TestBase.java
@@ -0,0 +1,38 @@
+package scenarios;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static utils.Attachments.*;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+        addListener("AllureSelenide", new AllureSelenide());
+        setupRemoteTestExecution();
+    }
+
+    @AfterEach
+    void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        attachVideo();
+        closeWebDriver();
+    }
+
+    private static void setupRemoteTestExecution() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+}
