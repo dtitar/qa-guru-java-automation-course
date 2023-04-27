552f81aaae20f4d12785f18c7bdd29055d7ea0c0
dtitar <dmitry.titar@gmail.com>
2023-04-27 15:19:16 +0300

    added lesson 5

---
+++ qag-5-selenide-allure/src/test/java/steps/Websteps.java
@@ -0,0 +1,39 @@
+package steps;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Websteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repository}")
+    public void searchForRepository(String repository) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repository);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Переходим в репозиторий {repository}")
+    public void goToRepository(String repository) {
+        $(By.linkText(repository)).click();
+    }
+
+    @Step("Переходим в таб Issues")
+    public void clickOnIssueTab() {
+        $(withText("Issues")).click();
+    }
+
+    @Step("Проверяем что Issue с номером {number} существует")
+    public void shouldSeeIssueWithNumber(int number) {
+        $(withText("#" + number)).should(exist);
+    }
+}
